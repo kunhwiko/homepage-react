6f298f95253dc3669d006b06b7dff27d341b7b43
Kun Hwi Ko <kokunhwi@gmail.com>
2021-05-27 02:41:28 -0700

    reconstruct Homepage.js to functional component

--- app/src/components/Homepage.js
+++ app/src/components/Homepage.js
@@ -3,48 +3,42 @@ import { Slide } from "react-slideshow-image";
 import 'react-slideshow-image/dist/styles.css';
 import '../style/homepage.css';
 
-export default class Homepage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div id="about" className="container">
-        <div className="left-split-container">
-          <h1>Crimebnb</h1>
-          <nav>
-            <ul>
-              <li><a className="title-text" href="/home">HOME</a></li>
-              <li><h2 className="title-text">/</h2></li>
-              <li><a className="title-text" href="/filteredListings">LISTINGS</a></li>
-              <li><h2 className="title-text">/</h2></li>
-              <li><a className="title-text" href="/neighborhoods">NEIGHBORHOODS</a></li>
-            </ul>
-          </nav>
-        </div>
-        <div className="right-split-container">
-          <div className="slide-container">
-            <Slide>
-              <div className="each-slide">
-                <div style={{'backgroundImage': 'url(assets/images/lease1.jpg)'}}>
+export default function Homepage() {
+  return (
+    <div id="about" className="container">
+      <div className="left-split-container">
+        <h1>Crimebnb</h1>
+        <nav>
+          <ul>
+            <li><a className="title-text" href="/home">HOME</a></li>
+            <li><h2 className="title-text">/</h2></li>
+            <li><a className="title-text" href="/filteredListings">LISTINGS</a></li>
+            <li><h2 className="title-text">/</h2></li>
+            <li><a className="title-text" href="/neighborhoods">NEIGHBORHOODS</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="right-split-container">
+        <div className="slide-container">
+          <Slide>
+            <div className="each-slide">
+              <div style={{'backgroundImage': 'url(assets/images/lease1.jpg)'}}>
                 <h1 className="slide-text1">Travel Safe</h1>
-                </div>
               </div>
-              <div className="each-slide">
-                <div style={{'backgroundImage': 'url(assets/images/lease2.jpg)'}}>
+            </div>
+            <div className="each-slide">
+              <div style={{'backgroundImage': 'url(assets/images/lease2.jpg)'}}>
                 <h1 className="slide-text2">Travel More</h1>
-                </div>
               </div>
-              <div className="each-slide">
-                <div style={{'backgroundImage': 'url(assets/images/lease3.jpg)'}}>
+            </div>
+            <div className="each-slide">
+              <div style={{'backgroundImage': 'url(assets/images/lease3.jpg)'}}>
                 <h1 className="slide-text3">Travel Further</h1>
-                </div>
               </div>
-            </Slide>
-          </div>
+            </div>
+          </Slide>
         </div>
       </div>
-    );
-  }
-}
+    </div>    
+  );
+} 
