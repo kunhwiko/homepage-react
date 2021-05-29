3ff8eecf8f6e0f2f00119888d3432dc98eb9e3f6
Kun Hwi Ko <kokunhwi@gmail.com>
2021-05-29 10:56:44 -0700

    rename class names in homepage

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -5,13 +5,13 @@ import '../style/about.css';
 export default function About() {
   return (
     <div id="home" className="split-container">
-      <div className="split-container-left">
+      <div className="split-container-left-about">
         <TextAnimation>
           <h1 className="author-text">Kun Hwi Ko</h1>
         </TextAnimation>
         <nav>
           <ul>
-            <li><a className="nav-text" href="/about">ABOUT</a></li>
+            <li><a className="nav-text" href="/home">HOME</a></li>
             <li><h2 className="nav-text">/</h2></li>
             <li><a className="nav-text" href="/work">WORK</a></li>
             <li><h2 className="nav-text">/</h2></li>
--- app/src/components/Homepage.js
+++ app/src/components/Homepage.js
@@ -8,24 +8,24 @@ import avatar from '../assets/images/avatar.jpeg';
 export default function Homepage() {
   return (
     <div id="home" className="split-container">
-      <div className="split-container-left">
+      <div className="split-container-left-home">
         <img alt="avatar" className="avatar" src={avatar} />
         <TextAnimation>
-          <h1 className="author-text">Kun Hwi Ko</h1>
+          <h1 className="author-text-home">Kun Hwi Ko</h1>
         </TextAnimation>
         <nav>
           <ul>
-            <li><a className="nav-text" href="/about">ABOUT</a></li>
-            <li><h2 className="nav-text">/</h2></li>
-            <li><a className="nav-text" href="/work">WORK</a></li>
-            <li><h2 className="nav-text">/</h2></li>
-            <li><a className="nav-text" href="/projects">PROJECTS</a></li>
-            <li><h2 className="nav-text">/</h2></li>
-            <li><a className="nav-text" href="/contact">CONTACT</a></li>
+            <li><a className="nav-text-home" href="/about">ABOUT</a></li>
+            <li><h2 className="nav-text-home">/</h2></li>
+            <li><a className="nav-text-home" href="/work">WORK</a></li>
+            <li><h2 className="nav-text-home">/</h2></li>
+            <li><a className="nav-text-home" href="/projects">PROJECTS</a></li>
+            <li><h2 className="nav-text-home">/</h2></li>
+            <li><a className="nav-text-home" href="/contact">CONTACT</a></li>
           </ul>
         </nav>
       </div>
-      <div className="split-container-right">
+      <div className="split-container-right-home">
         <Slide>
           <div className="each-slide">
             <div style={{'backgroundImage': 'url(assets/images/home1.jpg)'}}>
