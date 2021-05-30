eb17bab00ca24d6f69c9e0010311561d97e82d32
Kun Hwi Ko <kokunhwi@gmail.com>
2021-05-30 09:05:43 -0700

    update left container of about page

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -2,27 +2,26 @@ import React from 'react';
 import TextAnimation from "react-animate-text";
 import '../style/about.css';
 
-export default function About() {
+export default function Homepage() {
   return (
     <div id="home" className="split-container">
       <div className="split-container-left-about">
         <TextAnimation>
-          <h1 className="author-text">Kun Hwi Ko</h1>
+          <h1 className="author-text-about">Kun Hwi Ko</h1>
         </TextAnimation>
         <nav>
           <ul>
-            <li><a className="nav-text" href="/home">HOME</a></li>
-            <li><h2 className="nav-text">/</h2></li>
-            <li><a className="nav-text" href="/work">WORK</a></li>
-            <li><h2 className="nav-text">/</h2></li>
-            <li><a className="nav-text" href="/projects">PROJECTS</a></li>
-            <li><h2 className="nav-text">/</h2></li>
-            <li><a className="nav-text" href="/contact">CONTACT</a></li>
+            <li><a className="nav-text-about" href="/home">HOME</a></li>
+            <li><h2 className="nav-text-about">/</h2></li>
+            <li><a className="nav-text-about" href="/work">WORK</a></li>
+            <li><h2 className="nav-text-about">/</h2></li>
+            <li><a className="nav-text-about" href="/projects">PROJECTS</a></li>
+            <li><h2 className="nav-text-about">/</h2></li>
+            <li><a className="nav-text-about" href="/contact">CONTACT</a></li>
           </ul>
         </nav>
       </div>
-      <div className="split-container-right">
-
+      <div className="split-container-right-about">
 
       </div>
     </div>    
