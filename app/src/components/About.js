550a4afd9d3cd23ec01f28c444f6c92a11d315b9
Kun Hwi Ko <kokunhwi@gmail.com>
2021-05-30 20:22:17 -0700

    finish formatting text in about page

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -23,15 +23,16 @@ export default function Homepage() {
       </div>
       <div className="split-container-right-about">
         <h3 class="h3-about-text"><b>Software Engineer based in Silicon Valley</b></h3>
-        <h4 class="h4-about-text">I am starting a new journey at Moloco Ads on their infrastructure team. My work will contribute to building scalable servers and APIs that empower mobile app businesses through our cloud and machine learning technologies.</h4>
-        <h4 class="h4-about-text">Personally, I have a lot of interest in concurrency, scalability, failure handling, and API design. I also openly discuss systems design and various algorithms on GitHub. </h4>
+        <h4 class="h4-about-text">I am starting a new journey at C3.ai on their cloud infrastructure team. My work will contribute to building a scalable platform that supports applications for energy management, early disease detection, and predictive maintenance.</h4>
+        <h4 class="h4-about-text">Outside of work, I like to spend time dabbing into web and mobile development as a hobby. My most recent project is Crimebnb, a React.js app that allows users to view crime incidents near Airbnb listings in New York City.  </h4>
         <br></br>
-        <h3 class="h3-about-text"><b>Moving from Finance to Tech</b></h3>
-        <h4 class="h4-about-text">I came into computer science wanting to work in quant finance. Along the way, I met extraordinary people, gained valuable experiences, and took a detour that brought me into software engineering.</h4>
-        <h4 class="h4-about-text">I am fortunate enough to love what I do and am proud to be an engineer. Don't ever let change get the best of you and keep dreaming.</h4>
+        <h3 class="h3-about-text"><b>Transitioning from Finance to Tech</b></h3>
+        <h4 class="h4-about-text">I started programming wanting to work in quant trading. Along the way, I became fascinated with distributed systems and cloud computing, and took a detour that brought me into back-end engineering.   </h4>
+        <h4 class="h4-about-text">I am fortunate enough to have found something I truly enjoy doing and am proud to be an engineer. I am also fortunate to have met extraordinary people during my journey. Don't ever let change get the best of you.</h4>
         <br></br>
-        <h3 class="h3-about-text"><b>Investing in Stocks</b></h3>
-        <h4 class="h4-about-text">I like to research and learn about stocks in my free time. Currently, I am keeping an open eye out on Big Tech and the EV market.</h4>
+        <h3 class="h3-about-text"><b>Investing in Stocks and Cycling</b></h3>
+        <h4 class="h4-about-text">I like to research and learn about different markets, businesses, and stocks when I have time. Currently, I am keeping an open eye out on the EV market and cryptocurrency.</h4>
+        <h4 class="h4-about-text">As a short breather, I enjoy going for a ride on my bike. Usually, I don't have a particular destination in mind, I just go wherever the wind takes me.</h4>
       </div>
     </div>    
   );
