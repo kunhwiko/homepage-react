732011b3ee4bccb9cd6dbf70e415ad5650d578a4
Kun Hwi Ko <kokunhwi@gmail.com>
2021-06-11 03:18:28 -0700

    add button to overlay component

--- app/src/components/Project.js
+++ app/src/components/Project.js
@@ -25,29 +25,10 @@ export default function Project() {
       <div className="split-container-right-project">
         <div class="mini-row-container" style={{backgroundImage: "url(" + Movie + ")"}}>
           <div class="overlay fade1">
-            <h2 class="overlay-text">Movie Recommendation</h2>
-            <h3 class="overlay-text2">Python</h3>
-            <a class="button1" href="https://www.github.com/kunhwiko/movie-recommend"></a>
+            <h2 class="overlay-text">Crimebnb</h2>
+            <h3 class="overlay-text2">React.js, Node.js, MySQL</h3>
+            <a class="overlay-button" href="https://www.github.com/kunhwiko/crimebnb">>></a>
             <h4 class="overlay-text3">Anytime you're stuck on what movie you want to watch, use this recommendation system powered by machine learning.</h4>
-            <h4 class="overlay-text3">Simply put in movies you like, and wait for the output.</h4>
-          </div>
-        </div>
-        <div class="mini-row-container" style={{backgroundImage: "url(" + Movie + ")"}}>
-          <div class="overlay fade1">
-            <h2 class="overlay-text">Movie Recommendation</h2>
-            <h3 class="overlay-text2">Python</h3>
-            <a class="button1" href="https://www.github.com/kunhwiko/movie-recommend"></a>
-            <h4 class="overlay-text3">Anytime you're stuck on what movie you want to watch, use this recommendation system powered by machine learning.</h4>
-            <h4 class="overlay-text3">Simply put in movies you like, and wait for the output.</h4>
-          </div>
-        </div>
-        <div class="mini-row-container" style={{backgroundImage: "url(" + Movie + ")"}}>
-          <div class="overlay fade1">
-            <h2 class="overlay-text">Movie Recommendation</h2>
-            <h3 class="overlay-text2">Python</h3>
-            <a class="button1" href="https://www.github.com/kunhwiko/movie-recommend"></a>
-            <h4 class="overlay-text3">Anytime you're stuck on what movie you want to watch, use this recommendation system powered by machine learning.</h4>
-            <h4 class="overlay-text3">Simply put in movies you like, and wait for the output.</h4>
           </div>
         </div>
       </div>
