1b6057fdbaf57f13c069d6028ca445525106c4c0
Kun Hwi Ko <kokunhwi@gmail.com>
2021-06-12 07:55:10 -0700

    add crimebnb to project page

--- app/src/components/Project.js
+++ app/src/components/Project.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TextAnimation from "react-animate-text";
 import '../style/project.css';
+import House from '../assets/images/house.jpg';
 import Movie from '../assets/images/movie.jpg';
 
 export default function Project() {
@@ -23,12 +24,12 @@ export default function Project() {
         </nav>
       </div>
       <div className="split-container-right-project">
-        <div class="mini-row-container" style={{backgroundImage: "url(" + Movie + ")"}}>
+        <div class="mini-row-container" style={{backgroundImage: "url(" + House + ")"}}>
           <div class="overlay fade1">
             <h2 class="overlay-text">Crimebnb</h2>
             <h3 class="overlay-text2">React.js, Node.js, MySQL</h3>
             <a class="overlay-button" href="https://www.github.com/kunhwiko/crimebnb">>></a>
-            <h4 class="overlay-text3">Anytime you're stuck on what movie you want to watch, use this recommendation system powered by machine learning.</h4>
+            <h4 class="overlay-text3">Make your trip safer and smarter by viewing crime trends near Airbnb listings.</h4>
           </div>
         </div>
       </div>
