f51d44d89236886720ed748eabeed319ce2b5995
Kun Hwi Ko <kokunhwi@gmail.com>
2021-06-14 20:41:13 -0700

    add homepage react to project page

--- app/src/components/Project.js
+++ app/src/components/Project.js
@@ -3,6 +3,7 @@ import TextAnimation from "react-animate-text";
 import '../style/project.css';
 import House from '../assets/images/house.jpg';
 import Music from '../assets/images/music.jpg';
+import Homepage from '../assets/images/homepage.jpg';
 import Movie from '../assets/images/movie.jpg';
 
 export default function Project() {
@@ -28,7 +29,7 @@ export default function Project() {
         <div class="mini-row-container" style={{backgroundImage: "url(" + House + ")"}}>
           <div class="overlay fade1">
             <h2 class="overlay-text">Crimebnb</h2>
-            <h3 class="overlay-text2">React.js, Node.js, MySQL</h3>
+            <h3 class="overlay-text2">React.js, HTML5, CSS3, Node.js, MySQL</h3>
             <a class="overlay-button" href="https://www.github.com/kunhwiko/crimebnb">>></a>
             <h4 class="overlay-text3">Make your trip safer and smarter by viewing crime trends near Airbnb listings.</h4>
           </div>
@@ -41,8 +42,16 @@ export default function Project() {
             <h4 class="overlay-text3">Stream your music collection to others with our custom designed protocol.</h4>
           </div>
         </div>
-        <div class="mini-row-container" style={{backgroundImage: "url(" + Movie + ")"}}>
+        <div class="mini-row-container" style={{backgroundImage: "url(" + Homepage + ")"}}>
           <div class="overlay fade3">
+            <h2 class="overlay-text">Homepage</h2>
+            <h3 class="overlay-text2">React.js, HTML5, CSS3</h3>
+            <a class="overlay-button" href="https://www.github.com/kunhwiko/homepage-react">>></a>
+            <h4 class="overlay-text3">This amazing homepage is made in React and open source.</h4>
+          </div>
+        </div>
+        <div class="mini-row-container" style={{backgroundImage: "url(" + Movie + ")"}}>
+          <div class="overlay fade4">
             <h2 class="overlay-text">Newvies</h2>
             <h3 class="overlay-text2">Python</h3>
             <a class="overlay-button" href="https://www.github.com/kunhwiko/newvies">>></a>
